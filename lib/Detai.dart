7d8e7dede13776d4d7a8f5a5140d9db630c91369
GhadeBhavesh <bhaveshghade@gmail.com>
2023-04-14 21:15:57 +0530

    bhavesh

--- lib/Detai.dart
+++ lib/Detai.dart
@@ -16,76 +16,74 @@ class Detail extends StatefulWidget {
 }
 
 class _DetailState extends State<Detail> {
-  var _currentIndex;
-  final _pageController = PageController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        title: Text("LungCare"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                AuthenticationRepository.instance.signOutGoogle();
-              },
-              icon: Icon(Icons.logout_outlined))
-        ],
-        leading: Icon(Icons.health_and_safety),
-        elevation: 10,
-        backgroundColor: Colors.pink,
-      ),
-      body: PageView(
-          controller: _pageController,
-          onPageChanged: (index) {
-            setState(() => _currentIndex == 2);
-          },
-          children: [
-            Column(children: [
-              SizedBox(height: 40),
-              Image.asset(
-                "assets/hu4.png",
-                height: 120,
-              ),
-              SizedBox(
-                height: 50,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  '"We are going to detect cancer cells early and want to make India cancer free"',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Text(
-                  'Lung cancer is a type of cancer that begins in the lungs.Your lungs are two spongy organs in your chest that take in oxygen.Lung cancer is the leading cause of cancer deaths worldwide.Your lungs are two spongy organs in your chest that take in oxygen.Lung cancer is the leading cause of cancer deaths worldwide.',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                    fontSize: 16,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-              Container(
-                  height: 59,
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () => _pageController.jumpToPage(2),
-                    child: const Text("Start Predict"),
-                    style: ElevatedButton.styleFrom(
-                        primary: Colors.pink,
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(40))),
-                  )),
-            ]),
-          ]),
-    );
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text("LungCare"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  AuthenticationRepository.instance.signOutGoogle();
+                },
+                icon: Icon(Icons.logout_outlined))
+          ],
+          leading: Icon(Icons.health_and_safety),
+          elevation: 10,
+          backgroundColor: Colors.pink,
+        ),
+        body: SingleChildScrollView(
+                 child: Column(children: [
+                    SizedBox(height: 40),
+                    Image.asset(
+                      "assets/hu4.png",
+                      height: 120,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        '"We are going to detect cancer cells early and want to make India cancer free"',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Lung cancer is a type of cancer that begins in the lungs.Your lungs are two spongy organs in your chest that take in oxygen.Lung cancer is the leading cause of cancer deaths worldwide.Your lungs are two spongy organs in your chest that take in oxygen.Lung cancer is the leading cause of cancer deaths worldwide.',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    
+                    Container(
+                        height: 59,
+                        width: 200,
+                        child: ElevatedButton(
+                          onPressed: (){
+                            Navigator.push(context, MaterialPageRoute(builder: (context)=> Predict()));
+                          },
+                          child: const Text("Start Predict"),
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.pink,
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(40))),
+                        )),
+                        
+                  ]),
+                
+          ));
   }
 }
